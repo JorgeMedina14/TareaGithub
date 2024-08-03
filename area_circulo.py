8cf623884558143cdc06f5b2b588dd03c35b6e8c
JorgeMedina14 <jortiz15@miumg.edu.gt>
2024-08-02 21:27:18 -0600

    Version 1: Calculo basico de area de un circulo

---
+++ area_circulo.py
@@ -0,0 +1,8 @@
+def area_circulo_v1(radio):
+    pi = 3.14159
+    return pi * (radio ** 2)
+
+# Ejemplo de uso
+radio = 5
+area = area_circulo_v1(radio)
+print(f"Versión 1: El área del círculo con radio {radio} es {area}")
