6290ddca3c8ce4eb515c508e08ef892588ba3c14
JorgeMedina14 <jortiz15@miumg.edu.gt>
2024-08-02 21:29:17 -0600

    Versión 2: Usar la biblioteca math para obtener pi

--- area_circulo.py
+++ area_circulo.py
@@ -1,8 +1,10 @@
-def area_circulo_v1(radio):
-    pi = 3.14159
-    return pi * (radio ** 2)
+import math
+
+def area_circulo_v2(radio):
+    return math.pi * (radio ** 2)
 
 # Ejemplo de uso
 radio = 5
-area = area_circulo_v1(radio)
-print(f"Versión 1: El área del círculo con radio {radio} es {area}")
+area = area_circulo_v2(radio)
+print(f"Versión 2: El área del círculo con radio {radio} es {area}")
+
