b21fb8d21c077f1a9d9c4cecf8f3e4a0c195d378
JorgeMedina14 <jortiz15@miumg.edu.gt>
2024-08-02 21:33:50 -0600

    Versión 4: Usar anotaciones de tipo

--- area_circulo.py
+++ area_circulo.py
@@ -1,14 +1,12 @@
 import math
+from typing import Union
 
-def area_circulo_v3(radio):
-    try:
-        if radio < 0:
-            raise ValueError("El radio no puede ser negativo.")
-        return math.pi * (radio ** 2)
-    except TypeError:
-        print("El radio debe ser un número.")
+def area_circulo_v4(radio: Union[int, float]) -> float:
+    if radio < 0:
+        raise ValueError("El radio no puede ser negativo.")
+    return math.pi * (radio ** 2)
 
 # Ejemplo de uso
-radio = -5
-area = area_circulo_v3(radio)
-print(f"Versión 3: El área del círculo con radio {radio} es {area}")
+radio = 5
+area = area_circulo_v4(radio)
+print(f"Versión 4: El área del círculo con radio {radio} es {area}")
