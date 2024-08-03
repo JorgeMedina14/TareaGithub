901f5d89217dea1a69952ec40eb25a416110a98e
JorgeMedina14 <jortiz15@miumg.edu.gt>
2024-08-02 21:33:06 -0600

    Versión 3: Agregar manejo de errores

--- area_circulo.py
+++ area_circulo.py
@@ -1,10 +1,14 @@
 import math
 
-def area_circulo_v2(radio):
-    return math.pi * (radio ** 2)
+def area_circulo_v3(radio):
+    try:
+        if radio < 0:
+            raise ValueError("El radio no puede ser negativo.")
+        return math.pi * (radio ** 2)
+    except TypeError:
+        print("El radio debe ser un número.")
 
 # Ejemplo de uso
-radio = 5
-area = area_circulo_v2(radio)
-print(f"Versión 2: El área del círculo con radio {radio} es {area}")
-
+radio = -5
+area = area_circulo_v3(radio)
+print(f"Versión 3: El área del círculo con radio {radio} es {area}")
